47e41c0295e8d012001bbcf6c6326751c00d38b4
Dyl777 <dylpadaran77@gmail.com>
2026-02-07 09:10:16 +0100

    fix;- ./components/ui/resizable.tsx:5:10
    Type error: '"react-resizable-panels"' has no exported member named 'PanelGroup'. Did you mean 'PanelProps'? I've created stub components that don't depend on the problematic react-resizable-panels package. Since these components aren't actually used in your app, they're just placeholder implementations that will allow the build to succeed.

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -2,16 +2,22 @@
 
 import * as React from 'react'
 import { GripVerticalIcon } from 'lucide-react'
-import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels'
 
 import { cn } from '@/lib/utils'
+
+// Stub components - react-resizable-panels not currently used in the app
+// If you need to use these, install a compatible version: pnpm add react-resizable-panels@2.0.0
+
+interface ResizablePanelGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: 'horizontal' | 'vertical'
+}
 
 function ResizablePanelGroup({
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup>) {
+}: ResizablePanelGroupProps) {
   return (
-    <PanelGroup
+    <div
       data-slot="resizable-panel-group"
       className={cn(
         'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
@@ -23,20 +29,23 @@ function ResizablePanelGroup({
 }
 
 function ResizablePanel({
+  className,
   ...props
-}: React.ComponentProps<typeof Panel>) {
-  return <Panel data-slot="resizable-panel" {...props} />
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return <div data-slot="resizable-panel" className={className} {...props} />
+}
+
+interface ResizableHandleProps extends React.HTMLAttributes<HTMLDivElement> {
+  withHandle?: boolean
 }
 
 function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & {
-  withHandle?: boolean
-}) {
+}: ResizableHandleProps) {
   return (
-    <PanelResizeHandle
+    <div
       data-slot="resizable-handle"
       className={cn(
         'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
@@ -49,7 +58,7 @@ function ResizableHandle({
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
-    </PanelResizeHandle>
+    </div>
   )
 }
 
